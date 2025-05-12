3b88d28152fd0c9dd17f750c33a17077fcc1b83a
Tarun Monga <tarunmonga2208@gmail.com>
2025-05-12 17:40:21 +0530

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ const navbar = (
 const footer = <Footer>MIT {new Date().getFullYear()} © ForgeStack.</Footer>
 
 // Define sidebar order
-const sidebarOrder = ["introduction", "getting-started", "usage"];
+const sidebarOrder = ["introduction", "getting-started", "usage" ,"templates"];
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
 
